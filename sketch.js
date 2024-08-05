d121ca7d623787c21fa41c2a7ae451e7b5b495f2
Estefani <pivatto.estefani@escola.pr.gov.br>
2024-08-04 21:43:49 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+  background("white");
+     }
+  function draw() {
+  rect(30,20,20);
+  fill("pink");
+  stroke("black");
+  if(mouseIsPressed){
+  rect(mouseX,mouseY,20,20)}}
